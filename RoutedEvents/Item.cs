2e9a134a030ceee1e58b72c8d997683add92a407
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-05-03 14:54:29 +0200

    RoutedEvents

---
+++ RoutedEvents/Item.cs
@@ -0,0 +1,8 @@
+﻿namespace RoutedEvents
+{
+    public class Item
+    {
+        public string ControlName { get; set; }
+        public string EventName { get; set; }
+    }
+}
---
+++ RoutedEvents/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace RoutedEvents
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Item> Items { get; } = new ObservableCollection<Item>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            eventsDataGrid.ItemsSource = Items;
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            Items.Clear();
+        }
+
+        private void Element_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var element = sender as FrameworkElement;
+            Items.Add(new Item { EventName = "PrieviewMouseDown", ControlName = element.Name });
+
+            if (element.Name == GreenRectangle.Name)
+                e.Handled = true;
+        }
+
+        private void Element_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var element = sender as FrameworkElement;
+            Items.Add(new Item { EventName = "MouseDown", ControlName = element.Name });
+        }
+    }
+}
