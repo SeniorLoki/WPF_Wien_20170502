5b25d4bb4eeae940a8a33e72a479bdc1c49c5b05
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-05-04 14:02:13 +0200

    Validations

---
+++ Validation/ValidationRules/MustNotBe4ValidationRule.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace Validation.ValidationRules
+{
+    public class MustNotBe4ValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var text = value as string;
+
+            if (int.TryParse(text, out int zahl))
+            {
+                return zahl == 4 ?
+                        new ValidationResult(isValid: false, errorContent: "Value must not be 4!") :
+                        ValidationResult.ValidResult;
+            }
+
+            return new ValidationResult(false, "Could not be convertet.");
+        }
+    }
+}
